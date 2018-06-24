af2cecca6a602de527311802b841f5c110d7772b
Tim King <timcking@gmail.com>
2018-06-23 17:05:09 -0700

    Save before UI changes

--- app/src/main/java/com/timothyking/openmovie/MainActivity.java
+++ app/src/main/java/com/timothyking/openmovie/MainActivity.java
@@ -1,6 +1,7 @@
 package com.timothyking.openmovie;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,17 +29,19 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.timothyking.openmovie.MESSAGE";
     public  static final String TAG  = "OpenWeather";
     private EditText editSearch;
     Button buttonSearch;
     ListView mainListView;
     HashMap<Integer, String> hmapTitle = new HashMap<Integer, String>();
     HashMap<Integer, String> hmapMovieID = new HashMap<Integer, String>();
+    HashMap<Integer, String> hmapPoster = new HashMap<Integer, String>();
+    HashMap<Integer, String> hmapYear = new HashMap<Integer, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +51,31 @@ public class MainActivity extends AppCompatActivity {
         // Find the ListView resource.
         mainListView = findViewById( R.id.mainListView );
         buttonSearch = findViewById(R.id.buttonSearch);
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                getMovieItem(position);
+            }
+        });
+    }
+
+    public void getMovieItem(int position) {
+        String title = hmapTitle.get(position);
+        String movieid = hmapMovieID.get(position);
+        String poster = hmapPoster.get(position);
+        String year = hmapYear.get(position);
+
+        ArrayList<String> listStrings = new ArrayList<>();
+        listStrings.add(title);
+        listStrings.add(movieid);
+        listStrings.add(poster);
+        listStrings.add(year);
+
+        // Call new activity
+        Intent intent = new Intent(this, MovieDetail.class);
+        intent.putExtra(EXTRA_MESSAGE, listStrings);
+        startActivity(intent);
     }
 
     public void searchMovie (View view) {
@@ -54,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         String strSearch =  (editSearch.getText().toString());
 
         // Using string resource
-        String myURL = getString(R.string.search_url) + strSearch;
+        String myURL = getString(R.string.search_url) + "s=" + strSearch;
 
         DownloadTask task = new DownloadTask();
         task.execute(myURL);
@@ -127,10 +156,13 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonPart = arr.getJSONObject(i);
                     String title = jsonPart.getString("Title");
                     String imdbID = jsonPart.getString("imdbID");
+                    String poster = jsonPart.getString("Poster");
                     String year = jsonPart.getString("Year");
 
                     hmapTitle.put(i, title);
                     hmapMovieID.put(i, imdbID);
+                    hmapPoster.put(i, poster);
+                    hmapYear.put(i, year);
 
                     labelList.add(i, title + " (" + year + ")");
                 }
--- app/src/main/java/com/timothyking/openmovie/MovieDetail.java
+++ app/src/main/java/com/timothyking/openmovie/MovieDetail.java
@@ -1,13 +1,32 @@
 package com.timothyking.openmovie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
 
 public class MovieDetail extends AppCompatActivity {
+    TextView textTitle;
+    ImageView imagePoster;
+    TextView textYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        textTitle = (TextView) findViewById( R.id.textTitle);
+        imagePoster = (ImageView) findViewById(R.id.imagePoster);
+        textYear = (TextView) findViewById(R.id.textYear);
+
+        Intent intent = getIntent();
+        ArrayList<String> message = (ArrayList<String>) intent.getSerializableExtra(MainActivity.EXTRA_MESSAGE);
+
+        textTitle.setText(message.get(0));
+        Picasso.get().load(message.get(2)).into(imagePoster);
+        textYear.setText(message.get(3));
     }
 }
